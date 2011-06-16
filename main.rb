ee8d8ae4fa31f47b343679ea113f6efd6bb41e8f
Christian Sousa <neocsr@gmail.com>
2011-06-16 01:24:45 -0500

    Draft for main class working.

--- main.rb
+++ main.rb
@@ -1,21 +1,50 @@
 require 'rubygems'
 require 'thread'
+require 'lib/spider'
 
+# Config
+THREADS = 5
+SLEEP   = 1
+threads = []
+spiders = []
+ankens_with_error = []
+
+# Datasource
+URLS = [
+  {:anken_id => 1, :anken_title => "value1", :anken_url => "http://www.google.com" },
+  {:anken_id => 2, :anken_title => "value2", :anken_url => "http://www.google.com" },
+  {:anken_id => 3, :anken_title => "value1", :anken_url => "http://www.google.com" },
+  {:anken_id => 4, :anken_title => "value2", :anken_url => "http://www.google.com" },
+  {:anken_id => 5, :anken_title => "value1", :anken_url => "http://www.google.com" },
+  {:anken_id => 6, :anken_title => "value2", :anken_url => "http://www.google.com/test" }
+]
+
+# Queue
 queue = Queue.new
 
-
-5.times do |i|
-  sleep rand(i*2) # simulate expense
-  queue << i
-  puts "#{i} produced"
+URLS.each do |anken|
+  queue.push(anken)
 end
 
-consumer = Thread.new do
-  5.times do |i|
-    value = queue.pop
-    sleep rand(i/2) # simulate expense
-    puts "consumed #{value}"
-  end
+# Threads setup
+0.upto(THREADS - 1) do |i|
+    threads << Thread.new(i) do
+      spiders << Spider.new(i)
+      while queue.size > 0
+        anken = queue.pop
+        info = spiders[i].process_url(anken[:anken_url])
+        ankens_with_error << anken if info[:error]
+        sleep SLEEP
+      end
+    end
 end
 
-consumer.join
+puts "Here 1"
+
+threads.each do |th|
+  th.join
+end
+
+puts "Here 2"
+
+puts ankens_with_error.inspect
