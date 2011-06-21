86a20aa292581cb08ac7ef7493a828420df5be49
Christian Sousa <neocsr@gmail.com>
2011-06-21 14:40:22 -0500

    Updated datasource and smtp.

--- main.rb
+++ main.rb
@@ -6,64 +6,93 @@ require 'lib/generic_mailer'
 require 'active_record'
 
 # Config
-ENVIRONMENT = "development"
+ENVIRONMENT = "test"
 THREADS = 5
-SLEEP   = 1
+SLEEP   = 0.2
 threads = []
 spiders = []
 ankens_with_error = []
 
+start_time = Time.now
+
 # Datasource
+db_config = YAML::load_file("config/database.yml")
 
-URLS = [
-  {:anken_id => 1, :anken_title => "value1", :anken_url => "http://www.google.com" },
-  #{:anken_id => 2, :anken_title => "value2", :anken_url => "http://www.google.com" },
-  #{:anken_id => 3, :anken_title => "value3", :anken_url => "http://www.google.com/1234" },
-  #{:anken_id => 4, :anken_title => "value4", :anken_url => "http://www.google.com" },
-  #{:anken_id => 5, :anken_title => "value5", :anken_url => "http://www.google.com" },
-  #{:anken_id => 6, :anken_title => "value6", :anken_url => "http://www.google.com/test" }
-]
+ActiveRecord::Base.establish_connection(db_config[ENVIRONMENT])
 
-# Queue
-queue = Queue.new
+class SpiderUrl < ActiveRecord::Base
+  set_table_name "TBL_S_BLOCK_URL"
+  set_primary_key  "S_BLOCK_URL_ID"
+end
+
+URLS = SpiderUrl.find(:all)
+
+# Queues
+source_queue = Queue.new
+result_queue = Queue.new
 
 URLS.each do |anken|
-  queue.push(anken)
+  source_queue.push(anken.attributes) # Just save the hashes, not the AR objects.
 end
 
 # Threads setup
 0.upto(THREADS - 1) do |i|
-    threads << Thread.new(i) do
-      spiders << Spider.new(i)
-      while queue.size > 0
-        anken = queue.pop
-        info = spiders[i].process_url(anken[:anken_url])
-        ankens_with_error << anken if info[:error]
-        sleep SLEEP
-      end
+  threads << Thread.new(i) do
+    spiders << Spider.new(i)
+    while source_queue.size > 0
+      anken = source_queue.pop
+      info = spiders[i].process_url(anken["S_BLOCK_URL"])
+
+      puts info[:response_code]
+
+      ankens_with_error << anken.merge(info) if info[:error]
+      sleep SLEEP
     end
+  end
 end
 
 threads.each do |th|
   th.join
 end
 
-puts ankens_with_error.inspect
+
+attachment = "urls_check_#{Time.now.strftime('%Y%m%d')}.csv"
+file = File.open(attachment, 'w+')
+
+file.puts "URL ID, URL TITLE, URL, REGISTERED DATE, RESPONSE CODE, ERROR DETAIL "
+
+ankens_with_error.each do |anken|
+  row_data = "#{anken['S_BLOCK_URL_ID']}, #{anken['S_BLOCK_URL_NAME']}, " +
+             "#{anken['S_BLOCK_URL']}, " +
+             "#{anken['UP_DATE'] ? anken['UP_DATE'].strftime('%Y-%m-%d') : nil}, " +
+             "#{anken[:response_code]}, #{anken[:error_detail]}"
+  file.puts row_data
+end
+
+file.close
 
 # Send summary mail
 smtp_config = YAML::load_file("config/smtp.yml")
 
-from_address = "kevin@bitparagon.com"
-to_addresses = "neocsr@gmail.com"
-subject = "Summary"
+from_address = smtp_config[ENVIRONMENT]["from_address"]
+to_addresses = smtp_config[ENVIRONMENT]["to_addresses"]
+subject = "URL Check: #{Time.now.strftime('%Y-%m-%d')}"
 body = <<EOF
-Thank you for your attention.
+Summary
+=======
+
+Total Urls: #{URLS.size}
+Total Urls with Error: #{ankens_with_error.size}
+
+Report date: #{Time.now.strftime('%Y-%m-%d')}
+
 EOF
-attachment = "spec/energy.png"
 
 mailer = GenericMailer.new
 mailer.start(smtp_config[ENVIRONMENT])
 mailer.send_message(from_address, to_addresses, subject, body, attachment)
 mailer.finish
 
-puts "Report finished in xxx seconds."
+end_time = Time.now
+delay = end_time - start_time
+puts "Report in #{(delay/60).to_i}m #{delay % 60}s"
